df2f1d9bc7afe23dacddc42f2c99b7c203ef006d
Leonardo FRONT-END <97189523+leoDesenvolvedorWeb@users.noreply.github.com>
2023-12-29 12:42:49 -0300

    Ação js

---
+++ js/script-cadastro-produto.js
@@ -0,0 +1,20 @@
+let btnAdd = document.querySelector('#addBotao');
+
+//criar evento de click para o botão
+    btnAdd.addEventListener("click", function(){
+        let inputValor = document.querySelector('#meuinput').value;
+        if(inputValor === ""){
+            alert("VOCÊ NÃO PREENCHEU OS CAMPOS!")
+        }else{
+            //alert("CADASTRO REALIZADO COM SUCESSO")
+            document.querySelector("#mensagem-ok").insertAdjacentHTML(
+                "afterbegin",
+                `<div class="card-teste">
+                <span>CADASTRO EFETUADO COM SUCESSO!</span>
+                </div>
+                `
+            );
+        }
+
+        document.querySelector('#meuinput').value = "";
+    });
